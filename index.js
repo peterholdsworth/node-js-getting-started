3992564e08fc307858a465eb41cd9ae61917b460
Peter Holdsworth <peter.j.holdsworth@gmail.com>
2015-01-16 16:12:52 +0000

    Corrected addition of DYNO

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello Peter! My $DYNO is:', process.env.DYNO);
+  response.send('Hello Peter! My $DYNO is:' + process.env.DYNO);
 });
 
 app.listen(app.get('port'), function() {
